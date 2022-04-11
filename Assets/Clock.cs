5f3aafeaafe8113b2cf797564bf664b9a5d8ec1c
borbware <perttu.tuovinen@gmail.com>
2022-04-11 15:56:13 +0300

    Make clock beter :)

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -3,19 +3,21 @@ using System.Collections;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
-{
-    [SerializeField] Transform hoursObj, minutesObj, secondsObj;
-    [SerializeField] float hoursAngle, minutesAngle, secondsAngle;
-    public float time = 6 * 3600 + 30 * 60; // as seconds
-    [SerializeField] int hoursPrecision, minutesPrecision, secondsPrecision;
-    private int speed = 1;
+{  
 
+    [SerializeField] bool IsSmoothRotation;
+    [SerializeField] Transform hoursObj, minutesObj, secondsObj;
+    [SerializeField] int hoursPrecision, minutesPrecision, secondsPrecision;
+    [SerializeField] int speed = 1;
+    [SerializeField] float lerpSpeed = 0.1f;
+    float hoursAngle, minutesAngle, secondsAngle;
+    float time; // as seconds
     void Start()
     {
         time = DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second;
-        setSeconds(IndicatorPrecision(getSecondsAngle(DateTime.Now.Second),secondsPrecision));
+        setSeconds(IndicatorPrecision(getSecondsAngle(DateTime.Now.Second),     secondsPrecision));
         setMinutes(IndicatorPrecision(getMinutesAngle(DateTime.Now.Minute * 60),minutesPrecision));
-        setHours(IndicatorPrecision(getHoursAngle(DateTime.Now.Hour * 3600),hoursPrecision));
+        setHours(  IndicatorPrecision(getHoursAngle(DateTime.Now.Hour * 3600),  hoursPrecision));
     }
     private void setSeconds(float angle) {
         secondsAngle = angle;
@@ -29,7 +31,7 @@ public class Clock : MonoBehaviour
         hoursAngle = angle;
         hoursObj.localRotation = Quaternion.Euler(0,0,angle);
     }
-
+    
     private float getSecondsAngle(float time)
     {
         return ( time % 60 ) / 60 * 360; 
@@ -46,41 +48,44 @@ public class Clock : MonoBehaviour
     private bool isTimeToMove(float angle, float oldAngle, int precision)
     {
         float newAngle = IndicatorPrecision(angle, precision);
-        return (Mathf.DeltaAngle(oldAngle,newAngle) > 0);
+        return (Mathf.DeltaAngle(oldAngle, newAngle) > 0);
     }
     private float IndicatorPrecision(float angle, int precision)
     {
         var step = ( 360 / precision );
         return (float)Math.Floor( angle / step ) * step;
     }
-    void Update()
+    void FixedUpdate()
     {
-        time += Time.deltaTime * speed;
+        time += Time.fixedDeltaTime * speed;
+        if (IsSmoothRotation)
+        {
+            hoursObj.localRotation =   Quaternion.Euler(0,0,getHoursAngle(time));
+            minutesObj.localRotation = Quaternion.Euler(0,0,getMinutesAngle(time));
+            secondsObj.localRotation = Quaternion.Euler(0,0,getSecondsAngle(time));
+        } else {
+            if (isTimeToMove(getHoursAngle(time),hoursAngle,hoursPrecision))
+                StartCoroutine(
+                    LerpFunction(hoursAngle,
+                        IndicatorPrecision(getHoursAngle(time),hoursPrecision), 
+                    lerpSpeed / speed, setHours)
+                );
 
-        if (isTimeToMove(getHoursAngle(time),hoursAngle,hoursPrecision))
-            StartCoroutine(
-                LerpFunction(hoursAngle,
-                    IndicatorPrecision(getHoursAngle(time),hoursPrecision), 
-                0.1f / speed, setHours)
-            );
+            if (isTimeToMove(getMinutesAngle(time),minutesAngle,minutesPrecision))
+                StartCoroutine(
+                    LerpFunction(minutesAngle,
+                        IndicatorPrecision(getMinutesAngle(time),minutesPrecision),
+                    lerpSpeed / speed, setMinutes)
+                );
 
-        if (isTimeToMove(getMinutesAngle(time),minutesAngle,minutesPrecision))
-            StartCoroutine(
-                LerpFunction(minutesAngle,
-                    IndicatorPrecision(getMinutesAngle(time),minutesPrecision),
-                0.1f / speed, setMinutes)
-            );
+            if (isTimeToMove(getSecondsAngle(time),secondsAngle,secondsPrecision)) // second has passed
+                StartCoroutine(
+                    LerpFunction(secondsAngle,
+                        IndicatorPrecision(getSecondsAngle(time),secondsPrecision),
+                    lerpSpeed / speed, setSeconds)
+                );
+        }
 
-        if (isTimeToMove(getSecondsAngle(time),secondsAngle,secondsPrecision)) // second has passed
-            StartCoroutine(
-                LerpFunction(secondsAngle,
-                    IndicatorPrecision(getSecondsAngle(time),secondsPrecision),
-                0.01f / speed, setSeconds)
-            );
-
-        //hoursObj.localRotation = Quaternion.Euler(0,0,getHoursAngle(time));
-        //minutesObj.localRotation = Quaternion.Euler(0,0,getMinutesAngle(time));
-        //secondsObj.localRotation = Quaternion.Euler(0,0,secondsAngle(time)); // smooth rotation
     }
 
     IEnumerator LerpFunction(float startValue, float endValue, float duration, Action<float> func)
